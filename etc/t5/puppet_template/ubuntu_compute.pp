3f8986fd44f810d51956fa22673fbcd484284c89
xinwu <xin.wu@bigswitch.com>
2015-09-02 18:16:32 -0700

    BVS-4175, fix metadata not reachable

--- etc/t5/puppet_template/ubuntu_compute.pp
+++ etc/t5/puppet_template/ubuntu_compute.pp
@@ -336,7 +336,7 @@ if %(deploy_dhcp_agent)s {
         section           => 'DEFAULT',
         key_val_separator => '=',
         setting           => 'enable_metadata_network',
-        value             => 'True',
+        value             => 'False',
     }
     ini_setting { "dhcp agent disable dhcp_delete_namespaces":
         ensure            => present,
