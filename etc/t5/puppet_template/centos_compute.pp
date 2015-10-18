3b553da9bf19e4b0c0919f3a70dbdd4f8510ce7f
root <root@centos3.localdomain>
2015-10-18 01:13:49 -0700

    BVS-4335, p only with packstack kilo

--- etc/t5/puppet_template/centos_compute.pp
+++ etc/t5/puppet_template/centos_compute.pp
@@ -294,6 +294,9 @@ if %(deploy_l3_agent)s {
 }
 
 # ovs_neutron_plugin for packstack
+file { "/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini":
+    ensure  => file,
+}
 ini_setting { "disable tunneling":
   ensure            => present,
   path              => '/etc/neutron/plugins/openvswitch/ovs_neutron_plugin.ini',
