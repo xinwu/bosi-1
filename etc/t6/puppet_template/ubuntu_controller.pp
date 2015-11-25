da1f11f9bc6b13d258f63da68655f07dcdcd5306
xinwu <xin.wu@bigswitch.com>
2015-11-24 18:30:22 -0800

    fix typo

--- etc/t6/puppet_template/ubuntu_controller.pp
+++ etc/t6/puppet_template/ubuntu_controller.pp
@@ -348,7 +348,7 @@ ini_setting { "ml2 restproxy keystone_auth_url":
   path              => '/etc/neutron/plugins/ml2/ml2_conf.ini',
   section           => 'restproxy',
   key_val_separator => '=',
-  setting           => 'keystone_auth_url',
+  setting           => 'auth_url',
   value             => '%(keystone_auth_url)s',
   notify            => Service['neutron-server'],
 }
@@ -357,7 +357,7 @@ ini_setting { "ml2 restproxy keystone_auth_user":
   path              => '/etc/neutron/plugins/ml2/ml2_conf.ini',
   section           => 'restproxy',
   key_val_separator => '=',
-  setting           => 'keystone_auth_user',
+  setting           => 'auth_user',
   value             => '%(keystone_auth_user)s',
   notify            => Service['neutron-server'],
 }
@@ -366,7 +366,7 @@ ini_setting { "ml2 restproxy keystone_password":
   path              => '/etc/neutron/plugins/ml2/ml2_conf.ini',
   section           => 'restproxy',
   key_val_separator => '=',
-  setting           => 'keystone_password',
+  setting           => 'auth_password',
   value             => '%(keystone_password)s',
   notify            => Service['neutron-server'],
 }
@@ -375,7 +375,7 @@ ini_setting { "ml2 restproxy keystone_auth_tenant":
   path              => '/etc/neutron/plugins/ml2/ml2_conf.ini',
   section           => 'restproxy',
   key_val_separator => '=',
-  setting           => 'keystone_auth_tenant',
+  setting           => 'auth_tenant',
   value             => '%(keystone_auth_tenant)s',
   notify            => Service['neutron-server'],
 }
